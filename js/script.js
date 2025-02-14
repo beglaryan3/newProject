93f1a38521e4222e789f09788bd428a76235e7c9
beglaryan3 <sargisbeglaryan3@gmail.com>
2025-02-14 14:29:49 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".nav")
+const burgerMenu = document.querySelector(".burger_menu")
+
+burgerMenu.onclick = () => {
+    menu.classList.toggle("show")
+        
+}
