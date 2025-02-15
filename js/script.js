60353700598b9b864ad712c26929c423e2437c2b
beglaryan3 <sargisbeglaryan3@gmail.com>
2025-02-15 16:22:30 +0400

    aaa'
    git push origin main

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".nav")
-const burgerMenu = document.querySelector(".burger_menu")
-
-burgerMenu.onclick = () => {
-    menu.classList.toggle("show")
-        
-}
---
+++ main.js
@@ -0,0 +1,26 @@
+console.log(1)
+
+
+const menu = document.querySelector(".nav")
+const burgermenu =document.querySelector(".burger_menu")
+const menuClose =document.querySelector(".menu_close")
+// burgermenu.onclick =() =>{
+//     menu.classList.toggle("nav_show")
+
+//     if(menu.classList.contains("nav_show")){
+//         menuClose.style.display = "flex"
+//     }
+//     else{
+
+//         menuClose.style.display ="none"
+//     }
+    
+// }
+// menuClose.onclick =()=>{
+//     menu.classList.remove("nav_show")
+// }
+////////////////////
+
+const slider = document.querySelector(".slider")
+const sliderChildren = slider.children
+console.log(slider)
