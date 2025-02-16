0a95f9196dabd6e2d842116ce0226cb10fce8c9d
beglaryan3 <sargisbeglaryan3@gmail.com>
2025-02-16 14:27:16 +0400

    sxalnery dzvec

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 const menu = document.querySelector(".nav")
 const burgermenu =document.querySelector(".burger_menu")
 const menuClose =document.querySelector(".menu_close")
-// burgermenu.onclick =() =>{
-//     menu.classList.toggle("nav_show")
+burgermenu.onclick =() =>{
+    menu.classList.toggle("nav_show")
 
-//     if(menu.classList.contains("nav_show")){
-//         menuClose.style.display = "flex"
-//     }
-//     else{
+    if(menu.classList.contains("nav_show")){
+        menuClose.style.display = "flex"
+    }
+    else{
 
-//         menuClose.style.display ="none"
-//     }
+        menuClose.style.display ="none"
+    }
     
-// }
-// menuClose.onclick =()=>{
-//     menu.classList.remove("nav_show")
-// }
+}
+menuClose.onclick =()=>{
+    menu.classList.remove("nav_show")
+    menuClose.style.display = "none"
+}
 ////////////////////
 
-const slider = document.querySelector(".slider")
-const sliderChildren = slider.children
-console.log(slider)
+console.log(1)
 const swiper = new Swiper(".swiper", {});
